f3c8e9197fc67d2b8016350742fb4bee94bd9488
DESKTOP-QGS5I70\Fixer <crkingan@gmail.com>
2026-09-20 20:43:33 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     static void main() {
+        // Ints
+        int intOperandA = 2;
+        int intOperandB = 1;
+        int intSum = 3;
+        int intProduct = 2;
+        int intDifference = -1;
+        int intQuotient = 2;
+        int intModulo = 0;
 
+        //Doubles
+        double doubleOperandA = 5.5;
+        double doubleOperandB = 3.5;
+        double doubleSum = 8.5;
+        double doubleProduct = 19.25;
+        double doubleDifference = 2.0;
+        double doubleQuotient = 1.57;
+        double doubleModulo = 2.0;
+
+
+        //Questions
+        int kidsInFamily = 1;
+        String isItRaining = "yes";
+        double gallonOfGas = 4.25;
+        int favoriteNumber = 21;
+        double shoeSize = 10.5;
+        int birthMonth = 12;
+        String fullName = "Caleb Kingan";
+
+
+        System.out.println("Integer arithmatic");
+        System.out.println("The Sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        System.out.println("The Product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        System.out.println("The Difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        System.out.println("The Quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+        System.out.println("The Residue of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        System.out.println("\nDouble arithmatic");
+        System.out.println("The Sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        System.out.println("The Product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The Difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The Quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+        System.out.println("The Residue of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
+
+        System.out.println("\nQuestions");
+        System.out.println("the number of kids in your family: " + kidsInFamily);
+        System.out.println("the answer to is it raining?: " + isItRaining);
+        System.out.println("the price of a gallon of gas: " + gallonOfGas);
+        System.out.println("your favorite number: " + favoriteNumber);
+        System.out.println("your shoe size: " + shoeSize);
+        System.out.println("your birth month: " + birthMonth);
+        System.out.println("your full name: " + fullName);
     }
 }
